dfb7f04d8ac1ccb3ee7a479f35ee3c736052104f
Freakyab <aryanbhisikar1@gmail.com>
2022-12-28 23:16:18 +0530

    background updated

--- src/Body/App.js
+++ src/Body/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="main-div">
-        <div className="bg-style"></div>
-        <p className="main-data">hi</p>
+    <div className="bg">
+    </div>
+      <div className="main-div ">
+        <div className="bg-style "></div>
+        <p className="main-data ">hi</p>
         </div>
     </>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import "./style.css";
+import logo from "./logo.png"
 function Nav() {
     return (
         <>
             <nav>
-                <div className="logo_box"/>
+                {/* <div className="logo_box"/> */}
+                <img src={logo} className="logo_box" alt ="logo"/>
                 <ul>
                     <li className="contact"><a href="#Contact">Contact</a></li>
                     <li className="Gallery"><a href="#Gallery">Gallery</a></li>
