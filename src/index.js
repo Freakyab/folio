37b29e302694373632273d50f86108839dc0031e
Freakyab <aryanbhisikar1@gmail.com>
2023-03-28 00:53:42 +0530

    FIX ERROR

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import Nav from './components/Navbar/navbar'
-
+import Navbar from "./components/Navbar/navbar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Nav/>
+    <Navbar/>
 
   </React.StrictMode>
 );
