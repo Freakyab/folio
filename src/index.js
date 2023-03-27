5c32dab9eb5551008a02bf194b6db68a7d93e58c
Freakyab <aryanbhisikar1@gmail.com>
2023-03-28 00:58:21 +0530

    fix error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import Navbar from "./components/Navbar/navbar";
+import Navbar from "./components/navFolder/navbar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
